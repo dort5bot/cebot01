01bcba1fd482fb02c57b63545399e43f557e6f55
dort5bot <dersdep@gmail.com>
2025-08-01 19:32:00 +0300

    Update etf_job.py

--- jobs/etf_job.py
+++ jobs/etf_job.py
@@ -1,6 +1,13 @@
 # jobs/etf_job.py
-from utils.etf_utils import fetch_etf_data, save_etf_data_to_csv
 
-def etf_daily_job(context):
-    data = fetch_etf_data()
-    save_etf_data_to_csv(data)
+from telegram.ext import ContextTypes
+from datetime import time
+from utils.etf_utils import get_etf_data, save_etf_to_csv
+
+async def save_daily(context: ContextTypes.DEFAULT_TYPE):
+    etf_data = get_etf_data(["btc", "eth"])
+    save_etf_to_csv(etf_data)
+    print("ETF verisi CSV'ye kaydedildi.")
+
+def add_etf_job(job_queue):
+    job_queue.run_daily(save_daily, time(hour=18, minute=0))
