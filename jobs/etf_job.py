8f9908493cdec65f37521139c7b577d56cea2330
dort5bot <dersdep@gmail.com>
2025-08-01 19:01:24 +0300

    Update etf_job.py

--- jobs/etf_job.py
+++ jobs/etf_job.py
@@ -1,13 +1,6 @@
 # jobs/etf_job.py
-from utils.etf_utils import get_etf_snapshot
-from store.etf_store import load_etf_data, save_etf_data
-import datetime
+from utils.etf_utils import fetch_etf_data, save_etf_data_to_csv
 
-def etf_update_job(context):
-    data = load_etf_data()
-    snapshot = get_etf_snapshot()
-
-    now = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    data[now] = snapshot
-
-    save_etf_data(data)
+def etf_daily_job(context):
+    data = fetch_etf_data()
+    save_etf_data_to_csv(data)
