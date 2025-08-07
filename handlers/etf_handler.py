38c861d4c8ef573a37960ad113faf15281617858
dort5bot <dersdep@gmail.com>
2025-08-07 12:10:30 +0300

    Update etf_handler.py

--- handlers/etf_handler.py
+++ handlers/etf_handler.py
@@ -1,12 +1,13 @@
-# handlers/etf_handler.py
+# handlers/etf_handler.py farisi
 
 from telegram import Update
 from telegram.ext import ContextTypes
-from utils.etf_utils import get_etf_flow_report
+from utils.etf_utils import get_full_etf_report
 
 async def etf_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        report = await get_etf_flow_report()
+        report = await get_full_etf_report()
     except Exception as e:
         report = f"Hata oluştu: {e}"
     await update.message.reply_text(report)
+    
